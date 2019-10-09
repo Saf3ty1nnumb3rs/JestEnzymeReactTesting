6b290f5278f213b845f2315b873039419850176e
Josh Sample <joshua.sample1@globalpay.com>
2019-10-08 22:24:10 -0400

    Base set-up of app

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+
+const App = () => {
+    return (
+        <div>
+            <h2>Note To Self</h2>
+            <Form>
+                <FormControl />
+                <Button>
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
+};
+
+export default App;
